097136af69259489a2b49939013984982e24cb6c
helilong <2524143038@qq.com>
2019-11-25 11:08:49 +0800

    helilong

---
+++ js/index.js
@@ -0,0 +1,56 @@
+$(function(){
+		$(".ul>p").slideDown(3000);
+		
+		$("#sp1").mouseover(function(){
+			$("#sp1").addClass("topsp");
+		}).mouseout(function(){
+			$("#sp1").removeClass("topsp")
+		});
+		
+		$("#sp2").mouseover(function(){
+			$("#sp2").addClass("topsp");
+		}).mouseout(function(){
+			$("#sp2").removeClass("topsp")
+		});
+		
+		$("#sp3").mouseover(function(){
+			$("#sp3").addClass("topsp");
+		}).mouseout(function(){
+			$("#sp3").removeClass("topsp")
+		});
+		
+		$("#sp4").mouseover(function(){
+			$("#sp4").addClass("topsp");
+		}).mouseout(function(){
+			$("#sp4").removeClass("topsp")
+		});
+		
+		$("#sp5").mouseover(function(){
+			$("#sp5").addClass("topsp");
+		}).mouseout(function(){
+			$("#sp5").removeClass("topsp")
+		});
+		
+		$("#sp6").mouseover(function(){
+			$("#sp6").addClass("topsp");
+		}).mouseout(function(){
+			$("#sp6").removeClass("topsp")
+		});
+		
+		$("#sp7").mouseover(function(){
+			$("#sp7").addClass("topsp");
+		}).mouseout(function(){
+			$("#sp7").removeClass("topsp")
+		});
+	
+	   var sf = new Snowflakes({
+          color: "gray",
+          count: 100,
+          minOpacity: 0.3,
+          maxOpacity: 0.6
+        });
+	
+		
+		
+});
+
